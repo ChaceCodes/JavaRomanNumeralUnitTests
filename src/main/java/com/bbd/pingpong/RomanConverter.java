deafdfa645fcc8d25c105fe892237ad7ef80e4bd
Chace Green <chaceg@bbd.co.za>
2018-02-07 13:27:23 +0200

    Done

--- src/main/java/com/bbd/pingpong/RomanConverter.java
+++ src/main/java/com/bbd/pingpong/RomanConverter.java
@@ -2,10 +2,60 @@ package com.bbd.pingpong;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RomanConverter {
+    private static Pattern reFour = Pattern.compile("IV");
+    private static Pattern reFive = Pattern.compile("V");
+    private static Pattern reNine = Pattern.compile("IX");
+    private static Pattern reFourty = Pattern.compile("XL");
+    private static Pattern reFifty = Pattern.compile("L");
+    private static Pattern reNinety = Pattern.compile("XC");
+    private static Pattern reFourHundred = Pattern.compile("CD");
+    private static Pattern reFiveHundred = Pattern.compile("D");
+    private static Pattern reNineHundred = Pattern.compile("CM");
+    private static Pattern reTooManyRepeats = Pattern.compile("([M]{4,})|([D]{2,})|([C]{4,})|([L]{2,})|([X]{4,})|([V]{2,})|([I]{4,})");
+    private static Pattern reValidOrder = Pattern.compile("^M*C*X*I*$");
+
+    private static String SimplifyRomanNumeral(String s)
+    {
+        String simple;
+
+        simple = reFour.matcher(s).replaceAll("IIII");
+        simple = reFive.matcher(simple).replaceAll("IIIII");
+        simple = reNine.matcher(simple).replaceAll("IIIIIIIII");
+        simple = reFourty.matcher(simple).replaceAll("XXXX");
+        simple = reFifty.matcher(simple).replaceAll("XXXXX");
+        simple = reNinety.matcher(simple).replaceAll("XXXXXXXXX");
+        simple = reFourHundred.matcher(simple).replaceAll("CCCC");
+        simple = reFiveHundred.matcher(simple).replaceAll("CCCCC");
+        simple = reNineHundred.matcher(simple).replaceAll("CCCCCCCCC");
+
+        return (simple);
+    }
+
     public static int Convert(String s)
     {
-        return 1;
+        int num = 0;
+
+
+        if (reTooManyRepeats.matcher(s).find() == true)
+            throw new RuntimeException("Numeral contains too many repeats");
+        s = SimplifyRomanNumeral(s);
+        if (reValidOrder.matcher(s).find() == false)
+            throw new RuntimeException("Invalid numeral");
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s.charAt(i) == 'I')
+                num += 1;
+            else if (s.charAt(i) == 'X')
+                num += 10;
+            else if (s.charAt(i) == 'C')
+                num += 100;
+            else if (s.charAt(i) == 'M')
+                num += 1000;
+        }
+        return (num);
     }
 }
--- src/test/java/com/bbd/pingpong/testPing.java
+++ src/test/java/com/bbd/pingpong/testPing.java
@@ -12,5 +12,83 @@ public class testPing {
     public void testNumber1() {
         assertEquals(1,RomanConverter.Convert("I"));
     }
-
+    @Test
+    public void testNumber2() {
+        assertEquals(2,RomanConverter.Convert("II"));
+    }
+    @Test
+    public void testNumber5() {
+        assertEquals(5,RomanConverter.Convert("V"));
+    }
+    @Test
+    public void testNumber4() {
+        assertEquals(4,RomanConverter.Convert("IV"));
+    }
+    @Test
+    public void testNumber6() {
+        assertEquals(6,RomanConverter.Convert("VI"));
+    }
+    @Test
+    public void testNumber9() {
+        assertEquals(9,RomanConverter.Convert("IX"));
+    }
+    @Test
+    public void testNumber14() {
+        assertEquals(14,RomanConverter.Convert("XIV"));
+    }
+    @Test
+    public void testNumber29() {
+        assertEquals(29,RomanConverter.Convert("XXIX"));
+    }
+    @Test
+    public void testNumber49() {
+        assertEquals(49,RomanConverter.Convert("XLIX"));
+    }
+    @Test
+    public void testNumber90() {
+        assertEquals(90,RomanConverter.Convert("XC"));
+    }
+    @Test
+    public void testNumber100() {
+        assertEquals(100,RomanConverter.Convert("C"));
+    }
+    @Test
+    public void testNumber129() {
+        assertEquals(129,RomanConverter.Convert("CXXIX"));
+    }
+    @Test
+    public void testNumber400() {
+        assertEquals(400,RomanConverter.Convert("CD"));
+    }
+    @Test
+    public void testNumber500() {
+        assertEquals(500,RomanConverter.Convert("D"));
+    }
+    @Test
+    public void testNumber999() {
+        assertEquals(999,RomanConverter.Convert("CMXCIX"));
+    }
+    @Test
+    public void testNumber1999() {
+        assertEquals(1999,RomanConverter.Convert("MCMXCIX"));
+    }
+    @Test
+    void testInvalidChar()
+    {
+        assertThrows(RuntimeException.class, () -> RomanConverter.Convert("abc"));
+    }
+    @Test
+    void testInvalidRomanNumeral()
+    {
+        assertThrows(RuntimeException.class, () -> RomanConverter.Convert("IVXM"));
+    }
+    @Test
+    void testInvalidRomanNumeral2()
+    {
+        assertThrows(RuntimeException.class, () -> RomanConverter.Convert("XIIII"));
+    }@Test
+    void testInvalidRomanNumeral3()
+    {
+        assertThrows(RuntimeException.class, () -> RomanConverter.Convert("XVV"));
+    }
 }
